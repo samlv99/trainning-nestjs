a853e1d648b5c35c330a18a278317467a1c1aa64
samlv <samlv3112@gmail.com>
2022-08-31 23:33:49 +0700

    update1

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<h2>Hello World. I am machine. Very happy when doing the face enable</h2>';
+    return '<h2>Hello World. I am machine. Very happy when doing the face</h2>';
   }
 }
